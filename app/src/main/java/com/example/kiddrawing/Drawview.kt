2bc210b57fc3e82cddf26cf32a032e7f63c68783
ba063163 <farzad.norouzi@stud.uni-bamberg.de>
2021-12-08 10:55:51 +0100

    adding the brushSizeDialog in main Activity

--- app/src/main/java/com/example/kiddrawing/Drawview.kt
+++ app/src/main/java/com/example/kiddrawing/Drawview.kt
@@ -1,33 +1,40 @@
 package com.example.kiddrawing
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.MotionEvent
 import android.view.View
 
 class Drawview(context: Context, attr: AttributeSet) : View(context, attr) {
 
-    private val drawPath: CustomPath? = null
+    private var mdrawPath: CustomPath? = null
     private var mConvasBitMap: Bitmap? = null
     private var mDrawPaint: Paint? = null
     private var mConvasPaint: Paint? = null
     private var mBrushSize: Float = 0.toFloat()
     private val color = Color.BLACK
     private var canvas: Canvas? = null
+    private val mpaths=ArrayList<CustomPath>()
 
     init {
         setUpDrawing()
     }
-
+    /*we can not assign the value we get as parameter directly to the brush size because in this
+    case it can not adjust it self on different screen and devices which are gonna use this app
+    so we use this method called TypedValue.applyDimension()*/
+fun setBrushSize(size : Float){
+    mBrushSize=TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,size,
+    resources.displayMetrics)
+        mDrawPaint!!.strokeWidth=mBrushSize
+}
     /*
         The Paint class holds the style and color information about how to draw geometries, text and bitmaps
     */
     private fun setUpDrawing() {
         mDrawPaint = Paint()
-        mDrawPaint = CustomPath(color, mBrushSize)
+        mdrawPath = CustomPath(color, mBrushSize)
         mDrawPaint!!.color = color
         // shows the end of the line
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
@@ -38,26 +45,73 @@ class Drawview(context: Context, attr: AttributeSet) : View(context, attr) {
         /*  this flag is for the time we are drawing something and enable the moving the pixle
                   from one side to another place of memory .*/
         mConvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+       // mBrushSize = 20.toFloat()
+       /* we define a method called setBrushsize and with help of that we are gonna set the brush size in our
+                main activity*/
     }
 
-//    when our drawing view display this method will automatically run and set the configuration for us
+    //    when our drawing view display this method will automatically run and set the configuration for us
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 //        here is the place we declare our bitmap setting which we declare in form of canvas var
-        mConvasBitMap= Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888)
+        mConvasBitMap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
 //        the config ARGB_8888 will gives 256bit of color to each of our pixle on screen
 //        ARGB_8888 means each pixle can have color of 256 bit which means 193800000 different color
 //        can be chosen for each individual pixel
-        canvas=Canvas(mConvasBitMap!!)
+        canvas = Canvas(mConvasBitMap!!)
     }
 
-//    it runs when we want to draw something and it is exactly what we do we draw on our canvas
-    override fun onDraw(canvas: Canvas?) {
+    //    it runs when we want to draw something and it is exactly what we do we draw on our canvas
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        canvas.drawBitmap(mConvasBitMap!!, 0f, 0f, mConvasPaint)
+        for(path in mpaths){
+            mDrawPaint!!.strokeWidth = path.brushthickness
+            mDrawPaint!!.color = path.colokr
+            canvas.drawPath(path,mDrawPaint!!)
+        }
+        if (!mdrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mdrawPath!!.brushthickness
+            mDrawPaint!!.color = mdrawPath!!.colokr
+            canvas.drawPath(mdrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    //    in here we are gonna define what happen when we touch this event and as you know we wanna to draw something
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        val touchx = event?.x
+        val touchy = event?.y
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mdrawPath!!.colokr = color
+                mdrawPath!!.brushthickness = mBrushSize
+
+                mdrawPath!!.reset()
+                mdrawPath!!.moveTo(touchx!!, touchy!!)
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                mdrawPath!!.lineTo(touchx!!, touchy!!)
+            }
+            MotionEvent.ACTION_UP -> {
+                mpaths.add(mdrawPath!!)
+                mdrawPath = CustomPath(color, mBrushSize)
+            }
+            else -> {
+                return false
+            }
+        }
+        invalidate()
+
+        return true
 
     }
-    internal inner class CustomPath(var colokr: Int, var brushthickness: Float) : Paint() {
+
+
+    internal inner class CustomPath(var colokr: Int, var brushthickness: Float) : Path() {
 
     }
 }
+
+
--- app/src/main/java/com/example/kiddrawing/MainActivity.kt
+++ app/src/main/java/com/example/kiddrawing/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.kiddrawing
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    var drawingView: Drawview? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawingView = findViewById(R.id.view)
+//        in hwe are using the method we declared to set the brush size .
+        drawingView?.setBrushSize(20.toFloat())
+    }
+
+    private fun showBrushSizeSelectorDialog(){
+        val brushDialog=Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size : ")
+        val btnSmall=brushDialog
+
     }
 }
+
+
